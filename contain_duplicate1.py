59bd92d08b4dfee5ff4fe231dda3dbaeec50474b
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2022-12-30 12:43:47 +0530

    python solution

---
+++ contain_duplicate1.py
@@ -0,0 +1,13 @@
+def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s=set()
+        
+
+        for n in nums:
+            if n in s:
+                return True
+            s.add(n)
+        return False
