9c5218877e9ea48508c373f7d99d0ceabee5ff5e
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-01-03 13:54:30 +0530

    commit

---
+++ k_frequent_element.py
@@ -0,0 +1,23 @@
+def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+
+        for n in nums:
+            count[n]=1+count.get(n,0)
+        
+        for n, c in count.items():
+            freq[c].append(n)
+
+        result=[]
+
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                result.append(n)
+                if len(result)==k:
+                    return result
