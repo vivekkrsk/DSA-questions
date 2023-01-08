407278adea468f1e98e84e8834e0acdfcdf5bd02
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-01-08 14:08:00 +0530

    Update car_fleet.py

--- car_fleet.py
+++ car_fleet.py
@@ -6,4 +6,4 @@ def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         stack.append((target-p)/s)
         if len(stack)>=2 and stack[-1]<=stack[-2]:
                 stack.pop()
-        return len(stack)
+    return len(stack)
