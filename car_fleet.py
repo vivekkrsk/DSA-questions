9ac96f239681af76fd71375ae105864096d17fe9
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-01-08 14:09:16 +0530

    time complexity O(nlogn) and space complexity O(n)

--- car_fleet.py
+++ car_fleet.py
@@ -7,3 +7,4 @@ def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         if len(stack)>=2 and stack[-1]<=stack[-2]:
                 stack.pop()
     return len(stack)
+
