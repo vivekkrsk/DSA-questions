b11e7c45103aa7acac8c46525150990a18346407
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-01-18 13:11:06 +0530

    time complexity O(n) and space complecity O(n)

---
+++ longest_repeating_c_replacement_m2.py
@@ -0,0 +1,15 @@
+def characterReplacement(self, s: str, k: int) -> int:
+
+        count = {}
+        res,l =0,0
+        maxf=0
+
+        for r in range(len(s)):
+            count[s[r]]=1+count.get(s[r],0)
+            maxf = max(maxf, count[s[r]])
+
+            while(r-l+1)-maxf> k:
+                count[s[l]]-= 1
+                l+=1
+            res = max(res,r-l+1)
+        return res
