51ebeb44059a1a77d417cfa8b8e8328d062c3fb7
vivekkrsk <86180566+vivekkrsk@users.noreply.github.com>
2023-01-28 14:51:00 +0530

    backtracking soo;

---
+++ nqueen.py
@@ -0,0 +1,32 @@
+def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDig = set() # (r+c)
+        negDig = set() # (r-c)
+
+        res = []
+        board = [["."]*n for i in range(n)]
+
+        def backtrack(r):
+            if r==n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r+c) in posDig or (r-c) in negDig:
+                    continue
+
+                col.add(c)
+                posDig.add(r+c)
+                negDig.add(r-c)
+                board[r][c]="Q"
+
+                backtrack(r+1)
+
+                col.remove(c)
+                posDig.remove(r+c)
+                negDig.remove(r-c)
+                board[r][c]="."
+
+        backtrack(0)
+        return res
